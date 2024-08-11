58600058e11d4574c85319d7a45e9fba45aed080
RanaZaeem0 <ranazaeem34@gmail.com>
2024-08-11 18:35:56 +0500

    adada

---
+++ config.ts
@@ -0,0 +1,20 @@
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBOkFQbpd_fRueQnH6PXF5WbIP451tV6UU",
+  authDomain: "socailmedia-32217.firebaseapp.com",
+  projectId: "socailmedia-32217",
+  storageBucket: "socailmedia-32217.appspot.com",
+  messagingSenderId: "211457430319",
+  appId: "1:211457430319:web:9dea3c8ba658d3191bc7da"
+};
+
+// Initialize Firebase
+ const app = initializeApp(firebaseConfig);
+
+ export default app
